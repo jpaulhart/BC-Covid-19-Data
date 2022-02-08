700f3a4b20a6d8a5b90f2c32d5d1b1ba3a3fabd9
Paul Hart <jphart@glentech.com>
2022-02-08 14:02:58 -0800

    Updates

--- C19CollectData.sh
+++ C19CollectData.sh
@@ -32,20 +32,32 @@ echo
 echo -e $(date) "End C19 Collect Data complete -------------------------------------------"
 
 echo -e $(date) "Start C19 Collect Data (2)) -------------------------------------------"
+echo -e
 cd /Users/paulhart/Dropbox/Covid-19/C19DataDownload
+pwd
 
 echo -e $(date) 'Creating json data'
+echo -e
 /Users/paulhart/Dropbox/Covid-19/C19DataDownload/DerivedData/C19DataDownload/Build/Products/Debug/C19DataDownload /Users/paulhart/Dropbox/Covid-19/C19DataDownload/C19DataDownload/LocationData
 
 echo 
-echo -e $(date) "Commit files"
-git commit -m "Daily update $(date)"
-echo -e  $(date) 'Creating json data complete'
-echo 
+echo -e $(date) "Add files"
+echo -e
+echo -e "Add files"
+git add --all
+
 
 echo 
+echo -e $(date) "Commit files"
+echo -e
+git commit -m "Daily update $(date)"
+echo -e 
+echo -e  $(date) 'Creating json data complete'
+
+echo -e
 echo -e $(date) "Push files to Github"
-echo 
+echo -e
 git push -u origin main
+echo -e 
 
 echo -e $(date) "End C19 Collect Data complete (2) -------------------------------------------"
--- C19CollectData2.sh
+++ C19CollectData2.sh
@@ -3,10 +3,18 @@
 echo -e $(date) "Start C19 Collect Data (2)) -------------------------------------------"
 echo -e
 cd /Users/paulhart/Dropbox/Covid-19/C19DataDownload
+pwd
 
 echo -e $(date) 'Creating json data'
 echo -e
 /Users/paulhart/Dropbox/Covid-19/C19DataDownload/DerivedData/C19DataDownload/Build/Products/Debug/C19DataDownload /Users/paulhart/Dropbox/Covid-19/C19DataDownload/C19DataDownload/LocationData
+
+echo 
+echo -e $(date) "Add files"
+echo -e
+echo -e "Add files"
+git add --all
+
 
 echo 
 echo -e $(date) "Commit files"
