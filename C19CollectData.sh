faaf214965485d0455c597d6d064af2dcfb27464
Paul Hart <jphart@glentech.com>
2022-02-08 11:52:26 -0800

    Daily update Tue  8 Feb 2022 11:52:26 PST

--- C19CollectData.sh
+++ C19CollectData.sh
@@ -30,3 +30,21 @@ git push -u origin main
 
 echo
 echo -e $(date) "End C19 Collect Data complete -------------------------------------------"
+
+echo -e $(date) "Start C19 Collect Data (2)) -------------------------------------------"
+cd /Users/paulhart/Dropbox/Covid-19/C19DataDownload
+
+echo -e $(date) 'Creating csv data'
+/Users/paulhart/Dropbox/Covid-19/C19DataDownload/DerivedData/C19DataDownload/Build/Products/Debug/C19DataDownload /Users/paulhart/Dropbox/Covid-19/C19DataDownload/C19DataDownload/LocationData
+
+echo 
+echo -e $(date) "Commit files"
+git commit -m "Daily update $(date)"
+echo -e  $(date) 'Creating json data complete'
+echo 
+
+echo 
+echo -e $(date) "Push files to Github"
+git push -u origin main
+
+echo -e $(date) "End C19 Collect Data complete (2) -------------------------------------------"
